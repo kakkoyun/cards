cdf6dca00df7698e435e69c4672ca68a2f27be2d
Kemal Akkoyun <kakkoyun@gmail.com>
2017-03-26 16:11:01 +0300

    Init.

---
+++ lib/cards.ex
@@ -0,0 +1,31 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+  iex> Cards.hello
+  :world
+
+  """
+  def hello do
+    :world
+  end
+
+  @doc """
+    Say Hi!!
+
+    ## Examples
+
+    iex> Cards.say_hi
+    "hi there!"
+
+  """
+  def say_hi do
+    "hi there!"
+  end
+end
